150979fe0678d64c08acc93d7ea69a3f192980da
jaidevjoshi83 <jaidev53ster@gmail.com>
2021-04-06 11:40:12 -0400

    deleted build files

--- lib/plugin.d.ts
+++
@@ -1,7 +0,0 @@
-import { Application, IPlugin } from '@lumino/application';
-import { Widget } from '@lumino/widgets';
-/**
- * The example plugin.
- */
-declare const examplePlugin: IPlugin<Application<Widget>, void>;
-export default examplePlugin;
