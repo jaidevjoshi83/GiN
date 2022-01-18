7b50a656201b2d136dbfeaa19aef002456d219ca
Joshi <joshij@lri-108042.lerner.ad.cchs.net>
2022-01-18 08:13:39 -0500

    unused files  removed

--- js/lib/historydata.js
+++
@@ -1,4 +0,0 @@
-import '../style/historydata.css';
-import { GalaxyUIBuilderView } from './Galaxyuibuilder';
-import { ContextManager } from '@genepattern/nbtools';
-import { KernelSideDataObjects } from './utils';
